6a5afb996ae8c88271da47d7f78b5de9f2003a7b
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-01-25 19:07:33 -0500

    just starting

---
+++ app.js
@@ -0,0 +1,4 @@
+// Test-driven development- practice in which a developer creates failing test cases, and then writes code so that the tests pass. 
+// once the tests pass, the developer refactors the code to keep it DRY.
+// These steps are commonly referred to as Red, Green, Refactor.
+// write test first before a single line of code is written, and the write code in small increments.
